4e7c8c403c547d6c8df7f5cb1bacb4c8af981316
cccyb <chenyubo1996@gmail.com>
2017-03-07 18:46:02 +0800

    完成首页轮播图组件
    1.暂时只有页面结构，未做链接跳转

--- src/components/swipe.vue
+++ src/components/swipe.vue
@@ -1,33 +1,57 @@
 <template>
   <div class="swipe">
-    <mt-swipe :auto="4000">
-      <mt-swipe-item>
-        <a href="#"><img src="../assets/images/1.jpg" alt="1">小事 · 爱无能</a>
-      </mt-swipe-item>
-      <mt-swipe-item>
-        <a href="#"><img src="../assets/images/2.jpg" alt="1"></a>
-      </mt-swipe-item>
-      <mt-swipe-item>
-        <a href="#"><img src="../assets/images/3.jpg" alt="1"></a>
-      </mt-swipe-item>
-      <mt-swipe-item>
-        <a href="#"><img src="../assets/images/4.jpg" alt="1"></a>
-      </mt-swipe-item>
-      <mt-swipe-item>
-        <a href="#"><img src="../assets/images/5.jpg" alt="1"></a>
+    <span class="hot-news">今日热闻</span>
+    <mt-swipe :auto="2000">
+      <mt-swipe-item v-for="topStory in topStories" :key="topStory.id">
+        <img :src="'http://read.html5.qq.com/image?src=forum&q=5&r=0&imgflag=7&imageUrl=' + topStory.image" :alt="topStory.id">
+        <span class="top-story-title">{{topStory.title}}</span>
       </mt-swipe-item>
     </mt-swipe>
   </div>
 </template>
 
 <script>
+  import axios from 'axios';
   export default {
+    data() {
+      return {
+        topStories: {}
+      };
+    },
+    created() {
+      // 获取最热新闻
+      axios.get('/news/latest')
+        .then(response => {
+          this.topStories = response.data.top_stories;
+          console.log(this.topStories);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
   };
+
 </script>
 
 <style lang="sass">
-.swipe
+.swipe 
   height: 220px;
-  img
+  color: #fff;
+  font-size: 20px;
+  .hot-news 
+    position: absolute;
+    left: 50%;
+    top: 15px;
+    margin-left: -48px;
+    z-index: 1;
+  img 
+    position: absolute;
+    bottom: -75px;
     width: 100%;
+  .top-story-title
+    position: absolute;
+    z-index: 2;
+    bottom: 0;
+    padding: 30px 20px;
+    line-height: 25px;
 </style>
