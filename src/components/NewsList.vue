90a6a18e866edc17a2e4817a78b6eba1b2fee6d7
cccyb <chenyubo1996@gmail.com>
2017-03-07 21:53:29 +0800

    添加多图标签

--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -3,7 +3,10 @@
     <ul class="list">
       <li class="list-item" v-for="story in stories" :key="story.id">
         <span class="item-title">{{story.title}}</span>
-        <img class="item-image" :src="'http://read.html5.qq.com/image?src=forum&q=5&r=0&imgflag=7&imageUrl=' + story.images[0]" :alt="story.title">
+        <div class="image-wrapper">
+          <img class="item-image" :src="'http://read.html5.qq.com/image?src=forum&q=5&r=0&imgflag=7&imageUrl=' + story.images[0]" :alt="story.title">
+          <i class="icon iconfont icon-duotu multipic" v-if="story.multipic">多图</i>
+        </div>
       </li>
     </ul>
   </div>
@@ -42,9 +45,24 @@
       width: 70%;
       padding-top: 15px;
       line-height: 20px;
-    .item-image
-      width: 75px;
-      height: 60px;
+    .image-wrapper
+      position: relative;
       float: right;
       padding-top: 15px;
+      .item-image
+        width: 75px;
+        height: 60px;
+      .multipic
+        display: block;
+        position: absolute;
+        right: 0;
+        bottom: 2px;
+        width: 40px;
+        height: 16px;
+        font-size: 12px;
+        color: #fff;
+        background: #000;
+        opacity: .7;
+        text-align: center;
+        line-height: 16px;
 </style>
