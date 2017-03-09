87615c347098ff5df502f2a862bc70c614a5be57
cccyb <chenyubo1996@gmail.com>
2017-03-09 21:22:55 +0800

    暂时完成新闻详情页组件
    1.内容图片防盗链问题还未解决
    2.未加底部菜单栏

--- src/pages/NewsDetail.vue
+++ src/pages/NewsDetail.vue
@@ -1,12 +1,49 @@
 <template>
   <div class="news-detail">
-    这是文章详细页面,文章id为: {{$route.params.id}}
+    <div class="top-wrapper">
+      <img :src="attachImageUrl(this.data.image)" :alt="this.data.title">
+      <span class="top-title">{{data.title}}</span>
+      <span class="image-source">图片：{{this.data.image_source}}</span>
+    </div>
+    <div class="body-wrap" v-html="this.data.body"></div>
   </div>
 </template>
 <script>
-export default {
+  import axios from 'axios';
+  export default {
+    data() {
+      return {
+        id: '',
+        data: {}
+      };
+    },
+    created() {
+      // 获得新闻id
+      this.id = this.$route.params.id;
 
-};
+      // 获取指定id新闻具体内容
+			axios.get('/news/' + this.id)
+				.then(response => {
+					this.data = this.attachBodyContent(response.data);
+				})
+				.catch(error => {
+					console.log(error);
+				});
+    },
+    methods: {
+      // 修改图片链接
+      attachImageUrl: function(srcUrl) {
+        if (srcUrl !== undefined) {
+          return srcUrl.replace(/http\w{0,1}:\/\/p/g, 'https://images.weserv.nl/?url=p');
+        }
+      },
+      attachBodyContent: function(data) {
+        data.body.replace(/src=['"]([^'"]+)[^>]*>/g, '<img src="https://images.weserv.nl/?url=p" />');
+        console.log(data.body);
+        return data;
+      }
+    }
+  };
 </script>
 <style lang="sass">
 @import "../assets/sass/components/NewsDetail.sass";
