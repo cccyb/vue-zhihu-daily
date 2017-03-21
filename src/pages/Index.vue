0ed5a5da2cd7ddf4584ece88f5d7c7dbe8848ae2
cccyb <chenyubo1996@gmail.com>
2017-03-21 10:24:22 +0800

    去除header组件，整合进index页面组件

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,23 +1,41 @@
 <template>
   <div class="index">
-    <v-header></v-header>
+    <sidebar :isShowSidebar="isShowSidebar" v-on:hideSidebar="toggleSidebar"></sidebar>
+    <header>
+      <i class="icon iconfont icon-fenlei" @click="toggleSidebar"></i>
+      <span class="hot-news">今日热闻</span>
+    </header>
     <swipe></swipe>
     <newsList></newsList>
   </div>
 </template>
 <script>
-  import Header from '../components/Header';
-  import Swipe from '../components/Swipe';
-  import NewsList from '../components/NewsList';
+import Swipe from '../components/Swipe';
+import NewsList from '../components/NewsList';
+import Sidebar from '../components/Sidebar';
 
-  export default {
-    components: {
-      'v-header': Header,
-      'swipe': Swipe,
-      'newsList': NewsList
+export default {
+  data() {
+    return {
+      isShowSidebar: false
+    };
+  },
+  created() {
+    this.isShowSidebar = false;
+  },
+  components: {
+    'swipe': Swipe,
+    'newsList': NewsList,
+    'sidebar': Sidebar
+  },
+  methods: {
+    // 切换侧边栏显示状态
+    toggleSidebar() {
+      this.isShowSidebar = !this.isShowSidebar;
     }
-  };
+  }
+};
 </script>
-<style lang="sass">
-
+<style lang="sass" scoped>
+@import "../assets/sass/components/Index.sass";
 </style>
